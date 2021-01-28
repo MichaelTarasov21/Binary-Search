d086cb5488dd7ee95766d3637947441ff0c2ceeb
Michael <Michael.Tarasov@tutanota.com>
2021-01-27 21:11:09 -0500

    Set up JavaScript to begin searching
    Signed-off-by: Michael <Michael.Tarasov@tutanota.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+document.getElementById("data").innerHTML = `Your dataset is ${data}`;
+
+function search() {
+    const searchterm = document.getElementById("input").nodeValue
+    if (isNaN(searchterm)){
+        alert("I only know how to search for numbers")
+        return;
+    }
+}
+document.getElementById("submit").addEventListener("click", search);
